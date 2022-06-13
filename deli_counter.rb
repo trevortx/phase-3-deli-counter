a4d2bd74e458ad5286475d0c792f779232969924
Trevor Vardeman <trevortx@gmail.com>
2022-06-13 01:07:13 -0700

    commit tree 8743ce22f1f8591bf100a9a906feebc7e9cc187d

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def line array
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+    i = 0
+    new_array = []
+    num_array = (1..array.length).to_a
+    while i < array.length
+      new_array << "#{num_array[i]}."
+      new_array << array[i]
+      i += 1
+    end
+    puts "The line is currently: #{new_array.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  place_in_line = katz_deli.index(name) + 1
+  puts "Welcome, #{name}. You are number #{place_in_line} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length > 0
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
